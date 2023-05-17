e4a3a7491d95a3fa48e8fbffffbd35d1fde7c3f2
JigarjeetSingh <Jigarjeet1217@gmail.com>
2023-05-17 19:45:04 +0530

    get route added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res, next) => {
+  return res.json({
+    route: '/',
+    message: 'This is welcome message',
+  });
+});
+
 app.listen(3000, (err) => {
   if (err) return 'Some error';
   console.log('server startted listening on port 3000');
