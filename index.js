0785648f87ff26485eed5e4a496a0da560f1fe34
JigarjeetSingh <Jigarjeet1217@gmail.com>
2023-05-17 19:40:04 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('My todo app');
+const express = require('express');
+const app = express();
+
+app.listen(3000, (err) => {
+  if (err) return 'Some error';
+  console.log('server startted listening on port 3000');
+});
